72cd98cd6ff04d346e83cc1b3835dc7f29023a8d
Alice <alicelopess.art@gmail,com>
2025-03-05 18:56:23 +0000

    Segundo exercicio

---
+++ ChangeString/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ChangeString
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("String: ");
+            string s = Console.ReadLine();
+
+            Console.WriteLine("Caráter: ");
+            char carater = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            Console.WriteLine(" " + texto.Replace(carater, 'x'));
+        }
+    }
+}
