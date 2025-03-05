c23e1d631b5c8009e19e4fed9d310f78295d2f5e
Alice <alicelopess.art@gmail,com>
2025-03-05 18:13:22 +0000

    Primeiro exercicio

---
+++ RightSize/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RightSize
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length >= 8)
+            {
+                Console.WriteLine("[EARLY STOP]");
+                return;
+            }
+
+            if (args.Length > 3)
+            {
+                Console.WriteLine(arg);
+            }
+        }
+    }
+}
