bee31764b025d41cdc1f2f9245dde20dc48249c0
Alice <alicelopess.art@gmail,com>
2025-03-05 20:38:24 +0000

    Quinto exercicio

---
+++ Pow2Until/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection.PortableExecutable;
+
+namespace Pow2Until
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            for (int i = 1; i <= (1 << 5); i = i << 1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        static void Main(string[] args)
+        {
+            PowersOf2Until5();
+            PowersOf2Until5();
+
+            Console.ReadLine();
+        }
+    }
+}
